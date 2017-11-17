85d79b7c12b26d9549e0722efc43088cf74a433a
rahulrainahomeee <rahulrainahome@gmail.com>
2017-11-17 19:59:02 +0530

    Initial Commit

--- app/src/main/java/com/product/pustak/UpdateProfileActivity.java
+++ app/src/main/java/com/product/pustak/UpdateProfileActivity.java
@@ -1,20 +1,107 @@
 package com.product.pustak;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class UpdateProfileActivity extends AppCompatActivity {
+
+    public static final String TAG = "UpdateProfileActivity";
+
+    /**
+     * Class private UI component(s).
+     */
+    private Spinner spWork = null;
+    private TextView etName = null;
+    private TextView etEmail = null;
+    private TextView etMobile = null;
+    private TextView etArea = null;
+    private TextView etCity = null;
+    private TextView etState = null;
+    private TextView etCountry = null;
+    private TextView etPostalCode = null;
+
+    /**
+     * Class private data member(s).
+     */
+    private FirebaseFirestore db = null;
+    private User user = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
 
+        etName = (TextView) findViewById(R.id.txt_name);
+        etEmail = (TextView) findViewById(R.id.txt_email);
+        etMobile = (TextView) findViewById(R.id.txt_mobile);
+        etArea = (TextView) findViewById(R.id.txt_area);
+        etCity = (TextView) findViewById(R.id.txt_city);
+        etState = (TextView) findViewById(R.id.txt_state);
+        etCountry = (TextView) findViewById(R.id.txt_country);
+        etPostalCode = (TextView) findViewById(R.id.txt_postal_code);
+        spWork = (Spinner) findViewById(R.id.spinner_work);
+        spWork.setAdapter(new WorkSpinnerAdapter(this, R.layout.item_spinner_textview, getResources().getStringArray(R.array.work)));
 
+    }
+
+    public void save(View view) {
 
 
     }
 
+    /**
+     * Method to check the validation for all data field(s).
+     *
+     * @return boolean true = valid, false = validation fail.
+     */
+    private boolean validate() {
+
+        boolean isValid = true;
+
+        user = new User();
+        user.setName(etName.getText().toString().trim());
+        user.setEmail(etEmail.getText().toString().trim());
+        user.setMobile(Integer.parseInt(etMobile.getText().toString().trim()));
+        user.setArea(etArea.getText().toString().trim());
+        user.setCity(etCity.getText().toString().trim());
+        user.setState(etState.getText().toString().trim());
+        user.setCountry(etCountry.getText().toString().trim());
+        user.setPostal(Integer.parseInt(etPostalCode.getText().toString().trim()));
+        user.setWork(((TextView) spWork.getSelectedView()).getText().toString());
+        user.setGeo("");
+        user.setPic("");
+        user.setRate(0.0f);
+
+        db.collection("users")
+                .add(user)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+
+                        Toast.makeText(UpdateProfileActivity.this, "Profile updated", Toast.LENGTH_SHORT).show();
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(UpdateProfileActivity.this, "Update fail", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        return true;
+    }
 
 }
---
+++ app/src/main/java/com/product/pustak/WorkSpinnerAdapter.java
@@ -0,0 +1,71 @@
+package com.product.pustak;
+
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class WorkSpinnerAdapter extends ArrayAdapter<String> {
+
+    public static final String TAG = "WorkSpinnerAdapter";
+
+    /**
+     * Class private data member(s).
+     */
+    private Activity mActivity = null;
+    private int mLayout = -1;
+    private String[] mArrItems = null;
+
+    public WorkSpinnerAdapter(@NonNull Activity activity, int layout, @NonNull String[] arr) {
+        super(activity, layout, arr);
+
+        this.mActivity = activity;
+        this.mLayout = layout;
+        mArrItems = arr;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        TextView textView = null;
+
+        if (convertView != null) {
+
+            textView = (TextView) textView;
+
+        } else {
+
+            textView = (TextView) mActivity.getLayoutInflater().inflate(R.layout.item_spinner_textview, null);
+        }
+
+        textView.setTextColor(Color.WHITE);
+        textView.setText(mArrItems[position].toString());
+        textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.icon_work, 0, 0, 0);
+        return textView;
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        TextView textView = null;
+
+        if (convertView == null) {
+
+            textView = (TextView) mActivity.getLayoutInflater().inflate(R.layout.item_spinner_textview, null);
+            textView.setTextColor(Color.BLACK);
+            textView.setPadding(20, 20, 4, 20);
+        } else {
+
+            textView = (TextView) convertView;
+        }
+
+        textView.setText(mArrItems[position].toString());
+        return textView;
+    }
+}
