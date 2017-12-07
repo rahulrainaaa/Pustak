f2fe69c45c021e0ccf7d3fc3abbe525113ae8424
rahulrainahomeee <rahulrainahome@gmail.com>
2017-12-07 19:11:04 +0530

    Initial Commit.

--- app/src/main/java/com/product/pustak/fragment/derived/ProfileFragment.java
+++ app/src/main/java/com/product/pustak/fragment/derived/ProfileFragment.java
@@ -3,11 +3,17 @@ package com.product.pustak.fragment.derived;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.product.pustak.R;
 import com.product.pustak.fragment.base.BaseFragment;
 
-public class ProfileFragment extends BaseFragment {
+public class ProfileFragment extends BaseFragment implements View.OnClickListener {
 
     public static ProfileFragment getInstance() {
 
@@ -15,22 +21,48 @@ public class ProfileFragment extends BaseFragment {
         return fragment;
     }
 
+
+    private TextView txtName = null;
+    private TextView txtMobile = null;
+    private TextView txtEmail = null;
+    private TextView txtArea = null;
+    private TextView txtCity = null;
+    private TextView txtState = null;
+    private TextView txtCountry = null;
+    private ImageButton iBtnEdit = null;
+
+    @Nullable
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Toast.makeText(getActivity(), "onCreate ProfileFragment", Toast.LENGTH_SHORT).show();
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.frag_my_profile, container, false);
+
+        txtName = (TextView) view.findViewById(R.id.txt_user_name);
+        txtMobile = (TextView) view.findViewById(R.id.txt_phone);
+        txtEmail = (TextView) view.findViewById(R.id.txt_email);
+        txtArea = (TextView) view.findViewById(R.id.txt_area);
+        txtCity = (TextView) view.findViewById(R.id.txt_city);
+        txtState = (TextView) view.findViewById(R.id.txt_state);
+        txtCountry = (TextView) view.findViewById(R.id.txt_country);
+        iBtnEdit = (ImageButton) view.findViewById(R.id.btn_edit_profile);
+
+        iBtnEdit.setOnClickListener(this);
+
+        return view;
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        Toast.makeText(getActivity(), "onDetach ProfileFragment", Toast.LENGTH_SHORT).show();
-    }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getActivity(), "onDestroy ProfileFragment", Toast.LENGTH_SHORT).show();
-    }
+    public void onClick(View view) {
 
+        if (view.getId() == R.id.btn_edit_profile) {
+
+            Toast.makeText(getActivity(), "Edit profile under development.", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            Toast.makeText(getActivity(), "Unhandled OnClickListener()", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
